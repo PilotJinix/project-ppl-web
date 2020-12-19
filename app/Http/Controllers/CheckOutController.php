be941b2a9c8766117f53b1228fd6726afdc21e29
PilotJinix <agungsantoso557@gmail.com>
2020-12-20 02:07:42 +0700

    up

--- app/Http/Controllers/CheckOutController.php
+++ app/Http/Controllers/CheckOutController.php
@@ -10,148 +10,148 @@ use App\User;
 
 class CheckOutController extends Controller
 {
-  public function checkoutScreen(Request $request, $id){
-		$session = $request->session()->get('username');
-		if ($session != null) {
-			$akun = DB::table('users')->where('username',$session)->first();
-			$user_id = $akun->id;
+    public function checkoutScreen(Request $request, $id){
+        $session = $request->session()->get('username');
+        if ($session != null) {
+            $akun = DB::table('users')->where('username',$session)->first();
+            $user_id = $akun->id;
 
-			$detail = DB::table('produk')->where('id',$id)->first();
+            $detail = DB::table('produk')->where('id',$id)->first();
 
-			$riwayat_pembelian = DB::table('detail_checkout')
-			->join('produk', 'detail_checkout.produk_id','=','produk.id')
-			->where('detail_checkout.user_id','=',$user_id)
-			->select('detail_checkout.*','produk.nama','produk.harga','produk.gambar')->latest()
-			->get();
+            $riwayat_pembelian = DB::table('detail_checkout')
+                ->join('produk', 'detail_checkout.produk_id','=','produk.id')
+                ->where('detail_checkout.user_id','=',$user_id)
+                ->select('detail_checkout.*','produk.nama','produk.harga','produk.gambar')->latest()
+                ->get();
 
-			return view('checkout',compact('akun','detail','riwayat_pembelian'));
-		}
+            return view('checkout',compact('akun','detail','riwayat_pembelian'));
+        }
 
-		return redirect()->route('login');
-	}
+        return redirect()->route('login');
+    }
 
-	public function checkout(Request $request, $id){
-		$session = $request->session()->get('username');
-		$akun = DB::table('users')->where('username',$session)->first();
-		$user_id = $akun->id;
+    public function checkout(Request $request, $id){
+        $session = $request->session()->get('username');
+        $akun = DB::table('users')->where('username',$session)->first();
+        $user_id = $akun->id;
 
-		$request->validate([
-			'nama_lengkap' => 'required|string|max:255',
-			'email' => 'required|string|email|max:255',
-			'no_hp' => 'required|string|max:15|min:9',
-			'alamat' => 'required|string|max:255',
-			'label_alamat' => 'max:255',
-			'kode_pos' => 'required|string|max:100',
-			'kota' => 'required|string|max:100',
-			'catatan_pembelian' => 'max:255',
-			'jumlah' => 'required|string|max:255',
-			'biaya_pengiriman' => 'required|string|max:255',
-			'diskon' => 'required|string|max:255',
-			'metode_pembayaran' => 'required|string|max:255',
-			'total_harga' => 'required|string|max:255',
-		]);
-		
-		/* Get Time with timezone Asia/Jakarta */
-		date_default_timezone_set("Asia/Jakarta");
-		$currentTime = time();
-		$hours = 1;
-		$secondsToAdd = $hours * (60 * 60);
+        $request->validate([
+            'nama_lengkap' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'no_hp' => 'required|string|max:15|min:9',
+            'alamat' => 'required|string|max:255',
+            'label_alamat' => 'max:255',
+            'kode_pos' => 'required|string|max:100',
+            'kota' => 'required|string|max:100',
+            'catatan_pembelian' => 'max:255',
+            'jumlah' => 'required|string|max:255',
+            'biaya_pengiriman' => 'required|string|max:255',
+            'diskon' => 'required|string|max:255',
+            'metode_pembayaran' => 'required|string|max:255',
+            'total_harga' => 'required|string|max:255',
+        ]);
 
-		$newTime = $currentTime + $secondsToAdd;
-		$date = date('d F Y H:i', $newTime);
+        /* Get Time with timezone Asia/Jakarta */
+        date_default_timezone_set("Asia/Jakarta");
+        $currentTime = time();
+        $hours = 1;
+        $secondsToAdd = $hours * (60 * 60);
 
-		$link = route('info-payment',$id);
-		$user = User::first();
-		$user->notify(new Checkout($link, $date));
+        $newTime = $currentTime + $secondsToAdd;
+        $date = date('d F Y H:i', $newTime);
 
-		$detail = DetailCheckOut::create([
-			'nama_penerima' => $request->nama_lengkap,
-			'email_penerima' => $request->email,
-			'no_hp_penerima' => $request->no_hp,
-			'alamat_penerima' => $request->alamat,
-			'label_alamat_penerima' => $request->label_alamat,
-			'kode_pos_penerima' => $request->kode_pos,
-			'kota_penerima' => $request->kota,
-			'catatan_pembelian' => $request->catatan_pembelian,
-			'user_id' => $user_id,
-			'produk_id' => $id,
-			'jumlah' => $request->jumlah,
-			'metode_pembayaran' => $request->metode_pembayaran,
-			'biaya_pengiriman' => $request->biaya_pengiriman,
-			'diskon' => $request->diskon,
-			'total_harga' => $request->total_harga,
-			'batas_pembayaran' => $date,
-			'time' => $newTime,
-		]);
-		
-		$produk = DB::table('produk')->where('id',$id)->first();
-		
-		$newStok = $produk->stok - $request->jumlah;
-		DB::table('produk')->where('id',$id)->update([
-			'stok' => $newStok,
-		]);
-		
-		return redirect()->route('payment',$detail->id);
-	}
+        $link = route('info-payment',$id);
+        $user = User::first();
+        $user->notify(new Checkout($link, $date));
 
-	public function checkoutDetail(Request $request,$id){
-		$session = $request->session()->get('username');
-		$akun = DB::table('users')->where('username',$session)->first();
-		$user_id = $akun->id;
+        $detail = DetailCheckOut::create([
+            'nama_penerima' => $request->nama_lengkap,
+            'email_penerima' => $request->email,
+            'no_hp_penerima' => $request->no_hp,
+            'alamat_penerima' => $request->alamat,
+            'label_alamat_penerima' => $request->label_alamat,
+            'kode_pos_penerima' => $request->kode_pos,
+            'kota_penerima' => $request->kota,
+            'catatan_pembelian' => $request->catatan_pembelian,
+            'user_id' => $user_id,
+            'produk_id' => $id,
+            'jumlah' => $request->jumlah,
+            'metode_pembayaran' => $request->metode_pembayaran,
+            'biaya_pengiriman' => $request->biaya_pengiriman,
+            'diskon' => $request->diskon,
+            'total_harga' => $request->total_harga,
+            'batas_pembayaran' => $date,
+            'time' => $newTime,
+        ]);
 
-		$riwayat_pembelian = DB::table('detail_checkout')
-		->join('produk', 'detail_checkout.produk_id','=','produk.id')
-		->where('detail_checkout.user_id','=',$user_id)->select('detail_checkout.*','produk.nama','produk.harga','produk.gambar')->latest()
-		->get();
+        $produk = DB::table('produk')->where('id',$id)->first();
 
-		$detail = DB::table('produk')
-		->join('detail_checkout', 'detail_checkout.produk_id','=','produk.id')
-		->where('detail_checkout.id','=',$id)
-		->get();
-		
-		return view('status-pembelian',compact('akun','riwayat_pembelian','detail'));
-	}
+        $newStok = $produk->stok - $request->jumlah;
+        DB::table('produk')->where('id',$id)->update([
+            'stok' => $newStok,
+        ]);
 
-	public function payment(Request $request,$id){
-		$session = $request->session()->get('username');
-		$akun = DB::table('users')->where('username',$session)->first();
-		$user_id = $akun->id;
+        return redirect()->route('payment',$detail->id);
+    }
 
-		$riwayat_pembelian = DB::table('detail_checkout')
-		->join('produk', 'detail_checkout.produk_id','=','produk.id')
-		->where('detail_checkout.user_id','=',$user_id)->select('detail_checkout.*','produk.nama','produk.harga','produk.gambar')->latest()
-		->get();
+    public function checkoutDetail(Request $request,$id){
+        $session = $request->session()->get('username');
+        $akun = DB::table('users')->where('username',$session)->first();
+        $user_id = $akun->id;
 
-		$detail = DB::table('detail_checkout')->where('id',$id)->first();
-		return view('payment',compact('akun','riwayat_pembelian','detail'));
-	}
+        $riwayat_pembelian = DB::table('detail_checkout')
+            ->join('produk', 'detail_checkout.produk_id','=','produk.id')
+            ->where('detail_checkout.user_id','=',$user_id)->select('detail_checkout.*','produk.nama','produk.harga','produk.gambar')->latest()
+            ->get();
 
-	public function diterima(Request $request, $id){
-		DB::table('detail_checkout')->where('id',$id)->update([
-			'status_checkout' => 'Diterima'
-		]);
+        $detail = DB::table('produk')
+            ->join('detail_checkout', 'detail_checkout.produk_id','=','produk.id')
+            ->where('detail_checkout.id','=',$id)
+            ->get();
 
-		return redirect()->route('info-payment',$id);
-	}
+        return view('status-pembelian',compact('akun','riwayat_pembelian','detail'));
+    }
 
-	public function struk(Request $request, $id){
-		$request->validate([
-			'struk' => 'required|file|image|mimes:jpg,jpeg,gif,png'
-		]);
+    public function payment(Request $request,$id){
+        $session = $request->session()->get('username');
+        $akun = DB::table('users')->where('username',$session)->first();
+        $user_id = $akun->id;
 
-		$img = $request->file('struk');
-		$nama_file = time()."_".$img->getClientOriginalName();
+        $riwayat_pembelian = DB::table('detail_checkout')
+            ->join('produk', 'detail_checkout.produk_id','=','produk.id')
+            ->where('detail_checkout.user_id','=',$user_id)->select('detail_checkout.*','produk.nama','produk.harga','produk.gambar')->latest()
+            ->get();
 
-		$tujuan_upload = "assets/images/struk/";
+        $detail = DB::table('detail_checkout')->where('id',$id)->first();
+        return view('payment',compact('akun','riwayat_pembelian','detail'));
+    }
 
-		DB::table('detail_checkout')->where('id',$id)->update([
-			'struk' => $nama_file,
-			'status_checkout' => 'Menunggu Konfirmasi',
-		]);
+    public function diterima(Request $request, $id){
+        DB::table('detail_checkout')->where('id',$id)->update([
+            'status_checkout' => 'Diterima'
+        ]);
 
-		$img->move($tujuan_upload,$nama_file);
+        return redirect()->route('info-payment',$id);
+    }
 
-		return redirect()->route('info-payment',$id);
-	}
+    public function struk(Request $request, $id){
+        $request->validate([
+            'struk' => 'required|file|image|mimes:jpg,jpeg,gif,png'
+        ]);
+
+        $img = $request->file('struk');
+        $nama_file = time()."_".$img->getClientOriginalName();
+
+        $tujuan_upload = "assets/images/struk/";
+
+        DB::table('detail_checkout')->where('id',$id)->update([
+            'struk' => $nama_file,
+            'status_checkout' => 'Menunggu Konfirmasi',
+        ]);
+
+        $img->move($tujuan_upload,$nama_file);
+
+        return redirect()->route('info-payment',$id);
+    }
 
 }
